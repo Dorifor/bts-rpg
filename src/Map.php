a28553e9e05b48612e2593081b5759f5811118a9
Mao <mao@google.com>
2021-02-20 20:17:49 +0100

    Changement de méthode pour générer la map : création d'une matrice et utilisation de celle-ci pour générer le string final

--- src/Map.php
+++ src/Map.php
@@ -44,6 +44,7 @@ class Map
 
     public function genererMap(): string
     {
+        // Create Title
         $map = Titre::createTitle('map', $this->longueur * 4, '-', Couleurs::RED, Couleurs::YELLOW);
         $map .= str_repeat(' ', 2);
         for ($i = 0; $i < $this->longueur; $i++) {
@@ -51,21 +52,37 @@ class Map
         }
         $map .= PHP_EOL;
 
+        $nextMap = $this->genererMapArray();
+
         for ($y = 0; $y < $this->hauteur; $y++) {
-            $map .= $this->genererLigne($y);
+            $map .= $this->genererLigneMatrice($y, $nextMap);
         }
         return $map;
     }
 
-    public function genererLigne(int $numeroLigne): string
+    public function genererLigneMatrice(int $numeroLigne, array $mapMatrice): string
     {
         $ligne = "";
         $ligne .= Couleurs::BLUE . sprintf('%02d', $numeroLigne) . Couleurs::RESET;
         for ($x = 0; $x < $this->longueur; $x++) {
-            $ligne .= self::SEPARATEUR . self::POSITION_VIDE;
+            $ligne .= self::SEPARATEUR . $mapMatrice[$numeroLigne][$x];
         }
         $ligne .= self::SEPARATEUR . PHP_EOL;
         return $ligne;
+    }
+
+    public function genererMapArray(): array
+    {
+        $mat = array();
+        for ($y = 0; $y < $this->hauteur; $y++) {
+            $ligne = array();
+            for ($x = 0; $x < $this->longueur; $x++) {
+                array_push($ligne, self::POSITION_VIDE);
+            }
+            array_push($mat, $ligne);
+        }
+        $mat[$this->posYHero][$this->posXHero] = self::POSITION_HERO;
+        return $mat;
     }
 
     public function getLongueur()
